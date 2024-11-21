7782b32c8074242aa173520593b6f3bf7c20d251
Saharkhiz <niloufar.saharkhiz@fda.hhs.gov>
2024-11-21 12:41:00 -0500

    up

--- code/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-sDir = "../"
-mitsuba_dir = "/projects01/VICTRE/elena.sizikova/code/mitsuba_setup/mitsuba3/build/python"
-
-sDir_materials = sDir + '/data/materials/'  # directory where the skin layer models will be stored
-sDir_layers_orig = sDir_materials + 'skin_layers/'
-sDir_layers_hair = sDir_materials + 'skin_layers_hair/'
-sDir_layers_bloodVessel = sDir_materials + 'skin_layers_bloodVessel/'
-sDir_lesion = sDir_materials + 'lesions/' # directory where the skin layer models will be stored
-param_dir =  sDir + 'data/param_lists/' # directory where the .csv files are stored
